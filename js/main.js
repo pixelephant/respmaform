40f41e23fb47f530761ae0ec4c37e911e64ff272
Pixelephant Media <pixelephant@pixelephant.hu>
2012-07-30 14:12:51 +0200

    no message

--- js/main.js
+++ js/main.js
@@ -13,7 +13,12 @@ $(document).ready(function(){
 	$("#form .clearfix a").click(function(){
 		$(this).addClass("active").siblings().removeClass("active");
 		if($(this).data("type") === "contact"){
-			
+			$("#budget-field").fadeOut();
+			$("#description-label").html("Message");
+		}
+		else{
+			$("#budget-field").fadeIn();
+			$("#description-label").html("Description of the work");	
 		}
 		return false;
 	})
