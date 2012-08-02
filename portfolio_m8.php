1b7bbd2a48264d3b863e8938696bc173dda6e075
Pixelephant Media <pixelephant@pixelephant.hu>
2012-08-02 15:51:31 +0200

    no message

--- portfolio_m8.php
+++ portfolio_m8.php
@@ -2,7 +2,7 @@
   <section id="content">
     <hgroup>
       <h1>M8 MINI CITY BUS</h1>
-      <h2>for the special needs of inner city transportation <span>school project @MOME</span></h2>
+      <h2>for the special needs of inner city transportation <span>school project @<a href="http://www.mome.hu">MOME</a></span></h2>
     </hgroup>
     <p id="portfolio-nav">
       <a class="prev" href="#"> <span aria-hidden="true">&laquo;</span> Previous</a>
@@ -10,7 +10,7 @@
       <a class="next" href="#">Next <span aria-hidden="true">&raquo;</span></a>
     </p>
     <p id="data">
-      Started in <b>2012</b> and took <b>3</b> months to finish.
+      Started in <b>2011</b> and took <b>2</b> months to finish.
     </p>
 
     <section id="project">
@@ -18,37 +18,42 @@
       <img src="img/portfolio/m8/m8-01.jpg" alt="">
       
       <div class="text">
-        <h3>Design</h3>
-        <p>With M8 I aimed to design a admirable bus using technology available today (in five years) that can perfectly fit into city environments. If people enjoy riding a bus, they will be more likely to use public transportation – and this means a cleaner, quieter, more livable city.</p>
+        <h3>Love your ride!</h3>
+        <p>M8 is an admirable bus using technology available today (in five years) that can perfectly fit into city environments. If people <strong>enjoy</strong> riding a bus, they will be more likely to use <strong>public transportation</strong> – and this means a cleaner, quieter, more <strong>livable city</strong>.</p>
+      </div>
+
+      <div class="text">
+        <h3>MINIMAL IMPACT</h3>
+        <p>Thanks to the large reflecting surfaces, the bus <strong>fits into its environment</strong>, while the elegant metal frame makes it <strong>unique and recognizable</strong>.</p>
       </div>
 
       <figure>
         <img src="img/portfolio/m8/m8-02.jpg" alt="">
-        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+        <p>In the interior, <a href="http://en.wikipedia.org/wiki/Augmented_reality">AR-screens</a> help the passengers in navigation.</p>
       </figure>
       <figure>
         <img src="img/portfolio/m8/m8-03.jpg" alt="">
-        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+        <p>Longer wheelbase makes more useful space in the interior, wider doors help faster passenger exchange. The central seating of the driver gives him better view.</p>
       </figure>
       <figure>
         <img src="img/portfolio/m8/m8-04.jpg" alt="">
-        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+        <p>Easy access, fast passenger exchange.</p>
       </figure>
       <figure>
         <img src="img/portfolio/m8/m8-05.jpg" alt="">
-        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+        <p>The front passenger area is for short-term passengers area with double-function seatings in the side (A). The rear passenger area is for longer trips allowing passengers to sit down conveniently (B).</p>
       </figure>
       <figure>
         <img src="img/portfolio/m8/m8-06.jpg" alt="">
-        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+        <p>Narrower turning circle is possible with turnable super single rear wheels and the fact that the front wheel cuts are bigger in z-direction, thus allowing the front wheels to turn almost 90 degrees.</p>
       </figure>
       <figure>
         <img src="img/portfolio/m8/m8-07.jpg" alt="">
-        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+        <p>Screens are integrated in the windshields.</p>
       </figure>
       <figure>
         <img src="img/portfolio/m8/m8-08.jpg" alt="">
-        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+        <p>Further information about this project on <a href="http://www.behance.net/gallery/M8-city-mini-bus/4481351">Behance</a></p>
       </figure>
     </section>
 
