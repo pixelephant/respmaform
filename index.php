22112d73c1b2930de0bfef16f6c855247c43f368
Pap Norbert <norbert.pap@pixelephant.hu>
2012-08-09 15:54:58 +0200

    cv

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
     </hgroup>
     <section id="members">
       <div class="member" id="peti">
-        <a href="#" class="cv">CV</a>
+        <a href="doc/peti_cv.pdf" class="cv">CV</a>
         <div data-picture data-alt="Molnár Péter">
           <div data-src="img/peti.png"></div>
           <div data-src="img/peti@2x.png" data-media="(min-device-pixel-ratio: 2.0)"></div>
@@ -55,7 +55,7 @@
         <p>Péter is an analytic designer who always looks at the big picture. He wants to approach perfection in all of his works.</p>
       </div>
       <div class="member" id="geza">
-        <a href="#" class="cv">CV</a>
+        <a href="doc/geza_cv.pdf" class="cv">CV</a>
         <div data-picture data-alt="Csire Géza">
           <div data-src="img/geza.png"></div>
           <div data-src="img/geza@2x.png" data-media="(min-device-pixel-ratio: 2.0)"></div>
